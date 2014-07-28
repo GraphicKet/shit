61f5d121f8ddf103b7061f035ff77282db4c0947
GraphicKet <ketchupgaming@outlook.com>
2014-07-28 16:39:55 +0200

    Update cl_shit.lua

--- cl_shit.lua
+++ cl_shit.lua
@@ -8,7 +8,7 @@ function test()
     DermaPanel:MakePopup()
     DermaPanel:Center()
     DermaPanel.Paint = function()
-       draw.RoundedBox(0, 0, 0, DermaPanel:GetWide(), DermaPanel:GetTall(), Color(65, 120, 65))
+       draw.RoundedBox(0, 0, 0, DermaPanel:GetWide(), DermaPanel:GetTall(), Color(0, 0, 0))
     end
      
     local DButton = vgui.Create( "DButton", DermaPanel )
@@ -19,7 +19,7 @@ function test()
             LocalPlayer():ConCommand("connect 5.231.196.72:27015")
     end
     DButton.Paint = function()
-       draw.RoundedBox(0, 0, 0, DButton:GetWide(), DButton:GetTall(), Color(70, 120, 70))
+       draw.RoundedBox(0, 0, 0, DButton:GetWide(), DButton:GetTall(), Color(0, 0, 0))
        draw.SimpleText("We have moved to a new ip. Press this button to connect.", "HUDNumber5", DButton:GetWide() / 2, 25, color_white, TEXT_ALIGN_CENTER )
     end
 end
